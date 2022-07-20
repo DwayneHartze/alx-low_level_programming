b77e3b4ab644cc0ff188f311afb01033dad3fb06
DwayneHartze <dwaynehartze31@gmail.com>
2022-07-20 04:21:20 -0700

    Task 0 commit 4

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,9 +4,8 @@
 /*
  * File: lists.h
  *
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
+ * Desc: Header file contains prototypes and definitions for all functions
+  */
 
 #include <stdlib.h>
 
@@ -15,8 +14,7 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for ALX project
+ * Desc: singly linked list node structure
  */
 typedef struct listint_s
 {
@@ -34,6 +32,5 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
