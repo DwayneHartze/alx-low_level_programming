c1ff56bff48d94f24e43cbe747fb7cb8e39fb8c1
DwayneHartze <dwaynehartze31@gmail.com>
2022-07-20 04:09:58 -0700

    Task 0 commit 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,10 @@
 // Online C compiler to run C program online
 #ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
-/**
+/*
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
